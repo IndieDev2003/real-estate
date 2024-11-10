5ee7a168cadb4e16e576fa5f208b7f9c4f843399
Gagan Suman <fbdgagansuman@gmail.com>
2024-11-10 16:52:37 +0530

    github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,13 @@ import React, { useEffect, useState } from "react";
 const Navbar = () => {
   const [mobileActive, setMobileActive] = useState(false);
 
-
   const showMobile = () => {
-    setMobileActive(true)
-  }
+    setMobileActive(true);
+  };
 
   useEffect(() => {
     // console.log(mobileActive)
-  },[mobileActive])
+  }, [mobileActive]);
   return (
     <div className="min-h-content flex md:flew-row items-center justify-between">
       {/* Logo Section */}
@@ -36,24 +35,34 @@ const Navbar = () => {
 
       {/* Side Section */}
       <div className="flex gap-2">
-        <img src={github} className="h-10 sm:h-12" alt="" />
-        <img src={menu} onClick={showMobile} className="h-10 sm:h-12 sm:hidden" alt="" />
+        <a href="https://github.com/indiedev2003" target="blank">
+          <img src={github} className="h-10 sm:h-12" alt="" />
+        </a>
+        <img
+          src={menu}
+          onClick={showMobile}
+          className="h-10 sm:h-12 sm:hidden"
+          alt=""
+        />
       </div>
 
       {/* Mobile Menu */}
-      <div className={`${mobileActive ? 'top-0 right-0':'hidden -right-[100%]'} sm:hidden z-20 absolute w-full h-full bg-white transition-all duration-1000 `}>
+      <div
+        className={`${
+          mobileActive ? "top-0 right-0" : "hidden -right-[100%]"
+        } sm:hidden z-20 absolute w-full h-full bg-white transition-all duration-1000 `}
+      >
         <img
           src={cross}
           className="mb-2 h-10 rotate-45 "
           alt=""
-          onClick={()=>setMobileActive(false)}
+          onClick={() => setMobileActive(false)}
         />
         <h2 className="py-2 text-lg w-ful pl-10 bg-gray-500">Home</h2>
         <h2 className="py-2 text-lg w-ful pl-10">About Us</h2>
         <h2 className="py-2 text-lg w-ful pl-10">Contact Us</h2>
         <h2 className="py-2 text-lg w-ful pl-10">Our Project</h2>
         <h2 className="py-2 text-lg w-ful pl-10">Collection</h2>
-        
       </div>
     </div>
   );
